629065e6185a2d5b5a0432a1dfb76beaa9465d87
Maciej Walczak <Maciekarp@users.noreply.github.com>
2023-03-17 17:32:54 -0400

    Fixed FrogAnim and added basic bug flight

--- Assets/Scripts/BugController.cs
+++ Assets/Scripts/BugController.cs
@@ -7,10 +7,25 @@ public class BugController : MonoBehaviour
     [SerializeField] private Animator anim;
 
     [SerializeField] private string bugType;
+    [SerializeField] private GameObject bugParent;
+    [SerializeField] private GameObject bugTarget;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float rotateSpeed = 10;
+    
+
+    // Chase target follows and rotates towards the target gameobject
+    private void ChaseTarget() {
+        Vector3 targetDir = (bugTarget.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(targetDir, Vector3.up);
+        if((bugTarget.transform.position - transform.position).magnitude > 0.1f) {
+            transform.position = transform.position + targetDir * Time.deltaTime * speed;
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
+        }
+    }
 
     // Update is called once per frame
     void Update() {
-        
+        ChaseTarget();
     }
 
     public string getType() {
@@ -18,6 +33,7 @@ public class BugController : MonoBehaviour
     }
 
     public void killBug() {
+        Destroy(bugParent);
         Destroy(this.gameObject);
     }
     public void freezeAnim() {
